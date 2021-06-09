b46947d8d7136a076fc2249042a4ad596fefecb8
perismemo <pmemoi2015@gmail.com>
2021-06-09 22:13:54 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text,Image, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <CarItem />
     </View>
   );
 }
@@ -18,4 +17,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
